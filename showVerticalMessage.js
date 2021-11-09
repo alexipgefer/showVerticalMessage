68a976843b1ed0ec150578f4709d24104f723339
Alex Ipgefer <alex.ipgefer@yandex.ru>
2021-11-09 21:31:12 +0300

    Fixed bag

---
+++ showVerticalMessage.js
@@ -0,0 +1,17 @@
+function showVerticalMessage(str) {
+    let newStr = '';
+    let i = 0;
+    if (str[0] === 'м') {
+      newStr += str[0].toUpperCase() + '\n';
+      i++;
+    }
+    while ( i < str.length) {
+        newStr += str[i] + '\n';
+        i++;
+        if (i > 9) {
+          return newStr;
+        }      
+    }
+    return newStr;
+  };
+  showVerticalMessage('Марафонdddddd')
